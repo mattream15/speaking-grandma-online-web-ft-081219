b4724fff6bc3146ec6b975b471817708da9e7e35
generous-assembler-9561 <matthewtimothyream@gmail.com>
2019-08-19 18:32:22 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -7,7 +7,7 @@ else phrase == phrase.upcase
 end
   return "NO, NOT SINCE 1938!"
   ""
-elseif phrase = "I LOVE YOU GRANDMA!"
+elseif phrase == "I LOVE YOU GRANDMA!"
 end-ofinput
 return "I LOVE YOU TOO PUMPKIN!"
 end
