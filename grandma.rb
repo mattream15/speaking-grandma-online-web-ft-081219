97cd00b94c618d54e17d09ef065362314628de9f
generous-assembler-9561 <matthewtimothyream@gmail.com>
2019-08-19 19:17:20 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -1,10 +1,10 @@
 def speak_to_grandma(phrase)
   if phrase == "I LOVE YOU GRANDMA!"
 return "I LOVE YOU TOO PUMPKIN!"
-if phrase.downcase == phrase
-  return "HUH?! SPEAK UP, SONNY!"
-  els phrase == phrase.upcase
+  elsif phrase == phrase.upcase
   return "NO, NOT SINCE 1938!"
+  else phrase.downcase == phrase
+    return "HUH?! SPEAK UP, SONNY!"
 end
 end
 
